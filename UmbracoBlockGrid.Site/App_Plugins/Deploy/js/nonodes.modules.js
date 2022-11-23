10247cec07027f828fe564b167b0a7d5f737b245
Warren Buckley <warren@umbraco.com>
2022-11-23 15:17:52 +0000

    Revert "Adds in Umbraco.Deploy.OnPrem 11.0.0-rc2"
    This reverts commit e25c361edf785beecca128b9d8671f62137252ce.

--- UmbracoBlockGrid.Site/App_Plugins/Deploy/js/nonodes.modules.js
+++
@@ -1,9 +0,0 @@
-angular.module('umbraco.nonodes',
-[
-    'umbraco.deploy.resources',
-    'umbraco.deploy.services',
-    'umbraco.deploy.components'
-]);
-angular.module('umbraco.deploy.resources', []);
-angular.module('umbraco.deploy.services', []);
-angular.module('umbraco.deploy.components', []);
