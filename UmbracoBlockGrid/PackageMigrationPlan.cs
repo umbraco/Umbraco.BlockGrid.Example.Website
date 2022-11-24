412458661aabefcce3cd41ba9f219b660aa12a6e
Warren Buckley <warren@umbraco.com>
2022-11-24 15:50:33 +0000

    Other project added to be packed up as a nuget package

---
+++ UmbracoBlockGrid/PackageMigrationPlan.cs
@@ -0,0 +1,11 @@
+﻿using Umbraco.Cms.Infrastructure.Packaging;
+
+namespace UmbracoBlockGrid
+{
+    public class PackageMigrationPlan : AutomaticPackageMigrationPlan
+    {
+        public PackageMigrationPlan() : base("Umbraco Block Grid Example Website")
+        {
+        }
+    }
+}
