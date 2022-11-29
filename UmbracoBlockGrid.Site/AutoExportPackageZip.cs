99c4510fe2c5d4d937207a1a0c8a03d80325ffc1
Warren Buckley <warren@umbraco.com>
2022-11-29 13:32:11 +0000

    Updates to content to match Niels suggestions from this morning

--- UmbracoBlockGrid.Site/AutoExportPackageZip.cs
+++ UmbracoBlockGrid.Site/AutoExportPackageZip.cs
@@ -1,4 +1,5 @@
-﻿using Umbraco.Cms.Core.Composing;
+﻿using System.IO;
+using Umbraco.Cms.Core.Composing;
 using Umbraco.Cms.Core.Events;
 using Umbraco.Cms.Core.Services;
 using uSync.BackOffice;
@@ -39,20 +40,21 @@ namespace UmbracoBlockGrid.Site
             // Such as content changed, doctypes etc
 
             // EEK still old ugly integer ids
-            var packageDef = _packingService.GetCreatedPackageById(1);
+            var packageDef = _packingService.GetCreatedPackageById(2);
 
-            // Returns a FULL path such as...
-            // C:\\Code\\UmbracoBlockGrid\\UmbracoBlockGrid.Site\\umbraco\\Data\\CreatedPackages\\Umbraco_Block_Grid_Example_Website\\package.zip
-            var packageZipExported = _packingService.ExportCreatedPackage(packageDef);
+            if(packageDef != null)
+            {
+                // Returns a FULL path such as...
+                // C:\\Code\\UmbracoBlockGrid\\UmbracoBlockGrid.Site\\umbraco\\Data\\CreatedPackages\\Umbraco_Block_Grid_Example_Website\\package.zip
+                var packageZipExported = _packingService.ExportCreatedPackage(packageDef);
 
-            var embeddedZipFile = Path.Combine("../UmbracoBlockGrid", "package.zip");
-            var embeddedZipFileAbsolutePath = Path.GetFullPath(embeddedZipFile);
+                var embeddedZipFile = Path.Combine("../UmbracoBlockGrid", "package.zip");
+                var embeddedZipFileAbsolutePath = Path.GetFullPath(embeddedZipFile);
 
-            // Copy file over the top of the one we are embdedding for the AutoPackageMigrationPlan
-            // In the UmbracoBlockGrid project (This is the one that is the Nuget package)
-            File.Copy(packageZipExported, embeddedZipFileAbsolutePath, true);
-
+                // Copy file over the top of the one we are embdedding for the AutoPackageMigrationPlan
+                // In the UmbracoBlockGrid project (This is the one that is the Nuget package)
+                File.Copy(packageZipExported, embeddedZipFileAbsolutePath, true);
+            }
         }
-
     }
 }
